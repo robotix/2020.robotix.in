33496807c2ee1448825724978df17ac2e49f1e28
Aditya Narayan <narayanaditya95@gmail.com>
2015-07-29 23:18:47 +0530

    Cleanup redundant code.

--- js/scripts.js
+++ js/scripts.js
@@ -193,23 +193,6 @@
             }
         });
 
-        $('.slider').each(function() {
-            $(this).find('.slider-item').first().addClass('slider-current-item').removeClass('hidden');
-            if ($(this).find('.slider-item').length > 1) {
-                $(this).closest('.speaker-item').find('.slider-next-item').removeClass('hidden');
-            }
-        });
-        $('.slider-next-item').click(function() {
-            var slider = $(this).closest('div');
-            var elem = slider.find('.slider-current-item').next();
-            if (elem.length) {
-                elem.addClass('slider-current-item').removeClass('hidden');
-                slider.find('.slider-current-item').first().removeClass('slider-current-item').addClass('hidden');
-            } else {
-                slider.find('.slider-item').first().addClass('slider-current-item').removeClass('hidden');
-                slider.find('.slider-current-item').last().removeClass('slider-current-item').addClass('hidden');
-            }
-        });
         $('.modal').on('hidden.bs.modal', function () {
             var iframe = $(this).find('iframe');
             iframe.attr('src', iframe.attr('src'));
