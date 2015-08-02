7336f08f41a626b1b16c65c62ece86b44e1c4040
Aditya Narayan <narayanaditya95@gmail.com>
2015-08-03 02:10:43 +0530

    Further compress all JPG resources.

--- scripts/optimise_jpg.sh
+++ scripts/optimise_jpg.sh
@@ -3,4 +3,4 @@
 echo "Optimizing all JPG files in img/ directory"
 echo "Please wait..."
 
-find . -name '*.jpg' -exec jpegoptim --all-progressive -t -f -m 40 {} \;
+find ../img/ -name '*.jpg' -exec jpegoptim --all-progressive --strip-all -t --max=40 --threshold=5 {} \;
--- scripts/optimise_png.sh
+++ scripts/optimise_png.sh
@@ -3,4 +3,4 @@
 echo "Optimizing all PNG files in img/ directory"
 echo "Please wait..."
 
-find ../img/ -name '*.png' -exec pngquant --ext .png --force {} \;
+find ../img/ -name '*.png' -exec pngquant --verbose --ext .png --quality 40-90 {} \;
